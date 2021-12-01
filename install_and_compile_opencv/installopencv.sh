30a8788e5c9826b5e73b7691b8618ba3382c7ebb
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-01 13:42:35 -0500

    fixed package installs command

--- install_and_compile_opencv/installopencv.sh
+++ install_and_compile_opencv/installopencv.sh
@@ -5,11 +5,11 @@ sudo apt update && sudo apt upgrade
 
 #make sure you give it run permission with sudo chmod +x installopencv.sh
 
-sudo apt install build-essential cmake git pkg-config libgtk-3-dev
-    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev
-    gfortran openexr libatlas-base-dev python3-dev python3-numpy
-    libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev
+sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+    libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
+    libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
+    gfortran openexr libatlas-base-dev python3-dev python3-numpy \
+    libtbb2 libtbb-dev libdc1394-22-dev libopenexr-dev \
     libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
 
 mkdir ~/opencv_build && cd ~/opencv_build
